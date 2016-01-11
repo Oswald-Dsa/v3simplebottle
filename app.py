0f5d18016a73851462b7a14f2308a2c4ddc7f02c
Oswald D'sa <Oswald-Dsa@users.noreply.github.com>
2016-01-10 20:16:45 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1>OpenShift Ninja - MODIFIED BY Oswald ON December 22 2015</h1>"
+    return "<h1>OpenShift Ninja - MODIFIED BY Oswald ON January 10th 2016</h1>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
